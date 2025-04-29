362a92d7ccefe91f90b29149f488da0fc475432f
Swathi <swathisenthil20@gmail.com>
2025-04-29 15:18:59 +0530

    post initialized

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Post from './component/features/post/Post'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Post/>
     </>
   )
 }
